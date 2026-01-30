df8af2322eb01b098e02685ea4ec514a8343fb9b
Pavlov Andrei <payu2006@mail.ru>
2026-01-30 23:57:44 +0300

    Something else

--- tree.py
+++ tree.py
@@ -30,3 +30,51 @@ plt.scatter(
 )
 plt.plot(range(-2, 5), range(4, -3, -1))
 plt.show()
+
+from sklearn.tree import DecisionTreeClassifier
+
+
+# Let’s write an auxiliary function that will return grid for further visualization.
+def get_grid(data):
+    x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
+    y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+    return np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+
+
+clf_tree = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=17)
+
+# training the tree
+clf_tree.fit(train_data, train_labels)
+
+# some code to depict separating surface
+xx, yy = get_grid(train_data)
+predicted = clf_tree.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+plt.pcolormesh(xx, yy, predicted, cmap="autumn")
+plt.scatter(
+    train_data[:, 0],
+    train_data[:, 1],
+    c=train_labels,
+    s=100,
+    cmap="autumn",
+    edgecolors="black",
+    linewidth=1.5,
+)
+
+import pydotplus  # pip install pydotplus
+from sklearn.tree import export_graphviz
+
+
+def tree_graph_to_png(tree, feature_names, png_file_to_save):
+    # needs graphviz to be installed
+    tree_str = export_graphviz(
+        tree, feature_names=feature_names, filled=True, out_file=None
+    )
+    graph = pydotplus.graph_from_dot_data(tree_str)
+    graph.write_png(png_file_to_save)
+
+
+tree_graph_to_png(
+    tree=clf_tree,
+    feature_names=["x1", "x2"],
+    png_file_to_save="topic3_decision_tree1.png",
+)
